afa300d723e5eb29d9e519dd7c0bc54bf68adf87
Jonathan.Auber <auber.jonathan@gmail.com>
2023-03-01 11:22:36 +0100

    Exemple de menu burger

---
+++ semaine_04/Menu Burger/burger.js
@@ -0,0 +1,4 @@
+let menu = document.querySelector('.menu');
+let top = document.querySelector('#top');
+let middle = document.querySelector('#middle');
+let bottom = document.querySelector('#bottom');
---
+++ semaine_04/Révision DOM/app.js
@@ -0,0 +1,41 @@
+
+
+// let h1 = document.querySelectorAll("h1");
+// console.log(h1);
+
+// for(let i = 0; i < h1.length; i++){
+
+//     h1[i].addEventListener("click", function(){
+//         this.innerHTML = "bonjour tous le monde";
+//     })
+// }
+// let popup = document.querySelector(".popup");
+// modifier du text
+// h1.innerHTML= "<span style='color:green'>Bonjour</span>";
+// h1.innerText= "Bonjour";
+
+// Modifier du style
+// h1.style.background= "yellow";
+// h1.style.marginTop = "200px";
+// h1.style.animation = "2s descend linear infinite";
+
+// modifier les class
+// h1.classList.add("surligne");
+// h1.classList.remove("toto");
+
+// modifier id
+// h1.id = " toto";
+// console.log(h1.id);
+
+
+// evenement
+
+// h1.addEventListener("click", disBonjour)
+
+
+// function disBonjour(){
+
+//     popup.innerHTML ="<p> je suis dans la div popup </p>";
+//     popup.style.animation = "2s descend infinite";
+//     popup.style.background= "grey";
+// }
