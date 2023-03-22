9f7fb1ac5c352cb3aa1d552d79be636ab17c3e96
Jonathan.Auber <auber.jonathan@gmail.com>
2023-03-22 15:49:51 +0100

    MaJ correction

--- semaine_07/demoEXpress/routes/signup.js
+++ semaine_07/demoEXpress/routes/signup.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const mysql = require('mysql');
+var connection = require('../database.js')
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 8889,
-  user: 'root',
-  password: 'root',
-  database: 'NodeForm'
-});
 
+// A l'appel de la page on affiche le formulaire
 router.get('/', (req, res) => {
   res.render('signup', { message: '', title: 'Bienvenue' });
 });
 
+//On recupère les entrées utilisateur (en post, donc dans le body)
 router.post('/', (req, res) => {
   const { email, password } = req.body;
 
@@ -23,17 +18,18 @@ router.post('/', (req, res) => {
     if (results.length > 0) {
       res.render('signup', { message: 'Un utilisateur avec cette adresse mail existe déjà' , title: 'Bienvenue' });
     } else {
-      // Hashage du mot de passe
+      // Hashage du mot de passe parametres: password_en_clair, nb de caracteres du salt, fonction fléchée)
       bcrypt.hash(password, 10, (err, hash) => {
         if (err) throw err;
 
         // Insertion de l'utilisateur dans la base de données
         connection.query('INSERT INTO users SET ?', { email, password: hash }, (error, results) => {
           if (error) throw error;
-
+          // On ecrit en session le "connecté = vrai" et l'email
           req.session.loggedIn = true;
           req.session.email = email;
 
+          // on revient sur l'accueil
           res.redirect('/');
         });
       });
